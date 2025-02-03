a101d4eb85d4b979afdf5e02ebb5d3be26f55c91
Ang. Stamatis <angelos_stamatis@outlook.com>
2025-02-03 22:47:24 +0200

    Multiply calculator created

--- src/gr/aueb/cf/ch3/MulCalculator.java
+++ src/gr/aueb/cf/ch3/MulCalculator.java
@@ -1,4 +1,18 @@
 package gr.aueb.cf.ch3;
 
 public class MulCalculator {
+
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 2;
+        int result = 0;
+
+        result = mul(num1, num2);
+
+        System.out.println("Mul: " + result);
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
 }
